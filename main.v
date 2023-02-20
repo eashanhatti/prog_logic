c2f9ac4a4cfe1891d2c0c8c5e05bcb3147bbe9fd
Eashan Hatti <eashanhatti@gmail.com>
2023-02-19 20:54:14 -0500

    Progress on switching to downsets

--- main.v
+++ main.v
@@ -6,32 +6,38 @@ Require Import Init.Datatypes.
 Require Import Coq.Logic.FunctionalExtensionality.
 Import ListNotations.
 Require Import Coq.Init.Nat.
-Require Import String.
+Require Import Coq.Init.Specif.
+Require Import Coq.Classes.RelationClasses.
+Require Import Coq.Relations.Relation_Definitions.
+Require Import Coq.Sets.Ensembles.
+Require Import Coq.Logic.PropExtensionality.
+Require Import Coq.Init.Logic.
+Set Allow StrictProp.
 
+Arguments exist {A} {P}.
 Open Scope program_scope.
 
-Set Implicit Arguments.
-
-Structure effect_sig : Type := mk_effect_sig {
-  label : Type;
-  par : label -> Type;
-  ar : label -> Type
+Record poset := {
+  carrier : Type;
+  ord : relation carrier;
+  ord_preorder : PreOrder ord;
+  ord_antisym : Antisymmetric carrier eq ord
 }.
 
-Definition handler (esig : effect_sig) : Type :=
-  forall l : label esig, forall x : par esig l, ar esig l.
-
-Inductive play (esig : effect_sig) (a : Type) : Type :=
-| Emp : play esig a
-| Ret (x : a) : play esig a
-| InvRes (l : label esig) (x : par esig l) (v : ar esig l) (p : play esig a) : play esig a.
+Record downset (pset : poset) := mk_downset {
+  has : carrier pset -> Prop;
+  closed x y : ord pset x y -> has y -> has x
+}.
+Arguments has {pset}.
+Arguments closed {pset}.
 
 Class monad (m : Type -> Type) : Type := {
-  funit {a} : a -> m a;
+  unit {a} : a -> m a;
   fmap {a b} : (a -> b) -> m a -> m b;
   mult {a} : m (m a) -> m a;
-  mult_funit_id {a} : @mult a ∘ funit = id;
-  mult_fmap_funit_id {a} : @mult a ∘ fmap funit = id;
+  id {a} : m a -> m a;
+  mult_unit_id {a} : @mult a ∘ unit = id;
+  mult_fmap_unit_id {a} : @mult a ∘ fmap unit = id;
   mult_assoc {a} : @mult a ∘ fmap mult = mult ∘ mult
 }.
 
@@ -44,197 +50,134 @@ Class additive_monad (m : Type -> Type) : Type := {
   plus_assoc {a} {x y z : m a} : plus x (plus y z) = plus (plus x y) z
 }.
 
-Definition play_funit {esig a} (x : a) : play esig a := Ret _ x.
-Fixpoint play_mult {a esig} (pp : play esig (play esig a)) : play esig a :=
-  match pp with
-    | Ret _ p => p
-    | Emp _ _ => Emp _ _
-    | InvRes l x v p => InvRes l x v (play_mult p)
-  end.
-Fixpoint play_fmap {a b esig} (f : a -> b) (p : play esig a) : play esig b :=
-  match p with
-  | Ret _ x => Ret _ (f x)
-  | Emp _ _ => Emp _ _
-  | InvRes l x v p1 => InvRes l x v (play_fmap f p1)
-  end.
+Record effect_sig := mk_effect_sig {
+  label : Type;
+  par : label -> Type;
+  ar : label -> Type
+}.
 
-Definition strat (esig : effect_sig) (a : Type) : Type := handler esig -> list (play esig a).
+Inductive event (esig : effect_sig) (a : Type) : Type :=
+| Ret (v : a) : event esig a
+| Inv (l : label esig) (x : par esig l) : event esig a
+| Res (l : label esig) (v : ar esig l) : event esig a.
+Arguments Ret {esig} {a}.
+Arguments Inv {esig} {a}.
+Arguments Res {esig} {a}.
 
-Definition strat_funit {esig a} (x : a) : strat esig a := fun _ => [ play_funit x ].
-Definition strat_fmap {esig a b} (f : a -> b) (s : strat esig a) : strat esig b := fun h => map (play_fmap f) (s h).
-Fixpoint prod {esig a} (p : play esig (strat esig a)) : strat esig a :=
-  fun h => match p with
-  | Ret _ s => s h
-  | Emp _ _ => [ Emp _ _ ]
-  | InvRes l x v p1 =>
-      let s1 := prod p1
-      in map (fun p2 => InvRes l x v p2) (s1 h)
-  end.
-Definition strat_mult {esig a} (s : strat esig (strat esig a)) : strat esig a :=
-  fun h => flat_map (fun l => prod l h) (s h).
+Definition play esig a := list (event esig a).
 
-Theorem strat_mult_funit_id {esig a} : @strat_mult esig a ∘ strat_funit = id.
+Inductive wf_play {esig a} : play esig a -> Prop :=
+| WfEmp : wf_play []
+| WfRet {x} : wf_play [Ret x]
+| WfInvRes {l x v p} : wf_play (Res l v :: Inv l x :: p)
+| WfInv {l x} : wf_play [Inv l x].
+
+Inductive ord_play {esig a} : relation (play esig a) :=
+| EmpPrefix {p} : ord_play [] p
+| ConsPrefix {p1 p2 e} (ord_p : ord_play p1 p2) : ord_play (e :: p1) (e :: p2).
+
+Lemma pfx_nil_is_nil {esig a} {p : play esig a} : ord_play p [] -> p = [].
 Proof.
-extensionality s.
-extensionality h.
-cbv.
-change (fix app (l m : list (play esig a)) {struct l} : list (play esig a) :=
-          match l with
-          | [] => m
-          | a0 :: l1 => a0 :: app l1 m
-          end) with (@app (play esig a)).
-rewrite app_nil_r.
+intro.
+inversion H.
 reflexivity.
 Qed.
 
-Lemma prod_fmap_funit_funit {esig a} {p : play esig a} : prod (play_fmap strat_funit p) = fun _ => [p].
+Lemma ord_play_refl_prf {esig a} : forall p : play esig a, ord_play p p.
 Proof.
-extensionality h.
+intro.
 induction p.
-trivial.
-simpl.
-trivial.
-simpl.
-rewrite IHp.
-trivial.
+exact EmpPrefix.
+exact (ConsPrefix IHp).
 Qed.
 
+Lemma ord_play_trans_prf {esig a} : forall p1 p2 p3 : play esig a, ord_play p1 p2 -> ord_play p2 p3 -> ord_play p1 p3.
+Proof.
+Admitted.
 
-Theorem strat_mult_fmap_funit_id {esig a} : @strat_mult esig a ∘ strat_fmap strat_funit = id.
+Local Instance ord_play_refl {esig a} : Reflexive (@ord_play esig a) := {
+  reflexivity := ord_play_refl_prf
+}.
+
+Local Instance ord_play_trans {esig a} : Transitive (@ord_play esig a) := {
+  transitivity := ord_play_trans_prf
+}.
+
+Local Instance ord_play_preorder {esig a} : PreOrder (@ord_play esig a) := {
+  PreOrder_Reflexive := ord_play_refl;
+  PreOrder_Transitive := ord_play_trans
+}.
+
+Lemma ord_play_antisym_prf {esig a} : forall p1 p2 : play esig a, ord_play p1 p2 -> ord_play p2 p1 -> p1 = p2.
+Proof.
+Admitted.
+
+Local Instance ord_play_antisym {esig a} : Antisymmetric (play esig a) eq (@ord_play esig a) := {
+  antisymmetry := ord_play_antisym_prf
+}.
+
+Definition play_poset (esig : effect_sig) (a : Type) : poset := {|
+  carrier := play esig a;
+  ord := ord_play;
+  ord_preorder := ord_play_preorder;
+  ord_antisym := ord_play_antisym
+|}.
+
+Definition strat (esig : effect_sig) (a : Type) : Type := downset (play_poset esig a).
+
+Inductive strat_unit_has {esig a} (x : a) : play esig a -> Prop :=
+| StratUnitRet : strat_unit_has x [Ret x]
+| StratUnitDownclose (p1 p2 : _) (ord : ord_play p1 p2) (h : strat_unit_has x p2) : strat_unit_has x p1.
+
+Definition strat_unit {esig a} (x : a) : strat esig a :=
+  mk_downset
+    (play_poset esig a)
+    (strat_unit_has x)
+    (StratUnitDownclose x).
+
+Inductive flat {esig a b} : event esig a -> event esig b -> Prop :=
+| FlatInv {l x} : flat (Inv l x) (Inv l x)
+| FlatRes {l v} : flat (Res l v) (Res l v).
+
+Inductive strat_prod_has {esig a} : play esig (strat esig a) -> play esig a -> Prop :=
+| StratProdRet {s p} (_ : has s p) : strat_prod_has [Ret s] p
+| StratProdCons {e1 e2 p1 p2} (is_flat : flat e1 e2) (_ : strat_prod_has p1 p2) : strat_prod_has (e1 :: p1) (e2 :: p2).
+
+Inductive strat_mult_has {esig a} (ss : strat esig (strat esig a)) : play esig a -> Prop :=
+| StratMult {p1 p2} (_ : has ss p1) (_ : strat_prod_has p1 p2) : strat_mult_has ss p2
+| StratMultDownclose (p1 p2 : _) (ord : ord_play p1 p2) (h : strat_mult_has ss p2) : strat_mult_has ss p1.
+
+Definition strat_mult {esig a} (ss : strat esig (strat esig a)) : strat esig a :=
+  mk_downset
+    (play_poset esig a)
+    (strat_mult_has ss)
+    (StratMultDownclose ss).
+
+Inductive strat_fmap_help_has {esig a b} (f : a -> b) : play esig a -> play esig b -> Prop :=
+| StratFmapHelpRet {x} : strat_fmap_help_has f [Ret x] [Ret (f x)]
+| StratFmapHelpCons {e1 e2 p1 p2} (is_flat : flat e1 e2) (_ : strat_fmap_help_has f p1 p2): strat_fmap_help_has f (e1 :: p1) (e2 :: p2).
+
+Inductive strat_fmap_has {esig a b} (f : a -> b) (s : strat esig a) : play esig b -> Prop :=
+| StratFmap {p1 p2} (_ : has s p1) (_ : strat_fmap_help_has f p1 p2) : strat_fmap_has f s p2
+| StratFmapDownclose (p1 p2 : _) (ord : ord_play p1 p2) (h : strat_fmap_has f s p2) : strat_fmap_has f s p1.
+
+Definition strat_fmap {esig a b} (f : a -> b) (s : strat esig a) : strat esig b :=
+  mk_downset
+    (play_poset esig b)
+    (strat_fmap_has f s)
+    (StratFmapDownclose f s).
+
+Inductive strat_id_has {esig a} : play esig a -> Prop :=
+| StratId {p} : strat_id_has p
+| StratIdDownclose (p1 p2 : _) (ord : ord_play p1 p2) (h : strat_id_has p2) : strat_id_has p1.
+
+Definition strat_id {esig a} (s : strat esig a) : strat esig a :=
+  mk_downset
+    (play_poset esig a)
+    strat_id_has
+    StratIdDownclose.
+
+Lemma strat_mult_unit_id {esig a} : @strat_mult esig a ∘ strat_unit = strat_id.
 Proof.
 extensionality s.
-extensionality h.
 unfold compose.
-Admitted.
-
-Lemma strat_mult_assoc {esig a} : @strat_mult esig a ∘ strat_fmap strat_mult = strat_mult ∘ strat_mult.
-Proof.
-Admitted.
-
-Local Instance monad_strat {esig} : monad (strat esig) := {
-  funit _ := strat_funit;
-  fmap _ _ := strat_fmap;
-  mult _ := strat_mult;
-  mult_funit_id _ := strat_mult_funit_id;
-  mult_fmap_funit_id _ := strat_mult_fmap_funit_id;
-  mult_assoc _ := strat_mult_assoc
-}.
-
-Definition strat_plus {esig a} (s1 s2 : strat esig a) (h : handler esig) := s1 h ++ s2 h.
-Notation "s1 + s2" := (fun h => strat_plus s1 s2 h)
-  (at level 50, left associativity).
-
-Definition strat_neu {esig a} : strat esig a := const [].
-
-Theorem strat_plus_neu_l {esig a} {s : strat esig a} : strat_neu + s = s.
-Proof.
-extensionality h.
-cbv.
-reflexivity.
-Qed.
-
-Theorem strat_plus_neu_r {esig a} {s : strat esig a} : s + strat_neu = s.
-Proof.
-extensionality h.
-cbv.
-change (fix app (l m : list (play esig a)) {struct l} : list (play esig a) :=
-          match l with
-          | [] => m
-          | a0 :: l1 => a0 :: app l1 m
-          end) with (@app (play esig a)).
-rewrite app_nil_r.
-reflexivity.
-Qed.
-
-Theorem strat_assoc {esig a} {s1 s2 s3 : strat esig a} : s1 + (s2 + s3) = (s1 + s2) + s3.
-Proof.
-extensionality h.
-cbv.
-change (fix app (l m : list (play esig a)) {struct l} : list (play esig a) :=
-          match l with
-          | [] => m
-          | a0 :: l1 => a0 :: app l1 m
-          end) with (@app (play esig a)).
-rewrite app_assoc.
-reflexivity.
-Qed.
-
-Local Instance additive_monad_strat {esig} : additive_monad (strat esig) := {
-  plus a := strat_plus;
-  neu a := strat_neu;
-  plus_neu_l _ _ := strat_plus_neu_l;
-  plus_neu_r _ _ := strat_plus_neu_r;
-  plus_assoc a s1 s2 s3 := strat_assoc
-}.
-
-Definition strat_bind {esig a b} (act : strat esig a) (k : a -> strat esig b) : strat esig b :=
-  strat_mult (strat_fmap k act).
-Notation "x <- act ;; k" := (strat_bind act (fun x => k))
-  (at level 61, act at next level, right associativity).
-Notation "act ;; k" := (strat_bind act (fun _ => k))
-  (at level 61, right associativity).
-
-Definition strat_compose {esig a b c} (f : a -> strat esig b) (g : b -> strat esig c) : (a -> strat esig c) :=
-    fun x => y <- f x ;; g y.
-Notation "f >=> g" := (strat_compose f g)
-  (at level 40, left associativity).
-
-Fixpoint strat_star {esig a} (act : strat esig a) (n : nat) : strat esig a :=
-  match n with
-  | S m => act ;; strat_star act m
-  | Z => strat_neu
-  end.
-(* Notation "act * n" := (strat_star act n)
-  (at level 40, left associativity). *)
-
-Definition ret {esig a} := @strat_funit esig a.
-
-Definition skip {esig} : strat esig unit := strat_funit tt.
-
-Definition assert {esig} (b : bool) : strat esig unit :=
-  if b then
-    strat_funit tt
-  else
-    strat_neu.
-
-Definition invoke {esig} (l : label esig) (x : par esig l) : strat esig (ar esig l) :=
-  fun h =>
-  let v := h l x
-  in [InvRes l x v (Ret _ v)].
-
-Definition strat_if {esig a} (b : bool) (act1 act2 : strat esig a) : strat esig a :=
-  (assert b ;; act1) + (assert (negb b) ;; act2).
-
-Definition run_strat {esig a} (h : handler esig) (s : strat esig a) : list (play esig a) := s h.
-
-Record pair (a : Type) := {
-  fst : a;
-  snd : a
-}.
-
-Definition unit_sig : effect_sig := {|
-  label := unit;
-  par := const unit;
-  ar := const unit;
-|}.
-
-Definition nat_sig : effect_sig := {|
-  label := unit;
-  par := const (pair nat);
-  ar := const bool
-|}.
-
-Definition nat_handler : handler nat_sig :=
-  fun _ p => fst p <? snd p.
-
-Eval compute in
-  run_strat
-    nat_handler
-    (b <- @invoke nat_sig tt {|fst := 33; snd := 22 |} ;;
-     strat_if b
-      (strat_funit "foo"%string)
-      (strat_funit "bar"%string)).
-
-Eval compute in strat_funit "foo"%string + strat_funit "bar"%string.
-
-Eval compute in ret 2.
